533a5a0476afd2c9872961b6800e030621004600
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2016-11-17 19:49:14 -0600

    linked up the core data, added a generateTestData function to start playing around with the UI

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -24,18 +24,44 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        generateTestData()
+        attemptFetch()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        configureCell(cell: cell, indexPath: indexPath)
+        
+        return cell
     }
 
+    func configureCell(cell: ItemCell, indexPath: IndexPath) {
+        
+        let item = controller.object(at: indexPath)
+        cell.configureCell(item: item)
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        
         return 0
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
+        
         return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
     func attemptFetch() {
@@ -45,6 +71,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         fetchRequest.sortDescriptors = [dateSort]
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        self.controller = controller
         
         do {
             try controller.performFetch()
@@ -79,8 +107,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             break
         case.update:
             if let indexPath = indexPath {
-                let cell = tableView.cellForRow(at: indexPath as! ItemCell)
-                // update the cell data
+                let cell = tableView.cellForRow(at: indexPath) as! ItemCell
+                configureCell(cell: cell, indexPath: indexPath)
             }
             break
         case.move:
@@ -96,10 +124,27 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
     }
     
     
-    
-    
-    
-    
+    func generateTestData(){
+        
+        let item = Item(context: context)
+        item.title = "New Macbook Pro"
+        item.price = 1800
+        item.details = "I can't wait for the September event, I hope they releae new MPBs!"
+        
+        
+        let item2 = Item(context: context)
+        item2.title = "Bose Headphones"
+        item2.price = 300
+        item2.details = "But man, it's so nice to be able to block out everyone with the noise cancelling tech."
+        
+        let item3 = Item(context: context)
+        item3.title = "Tesla Model S"
+        item3.price = 110000
+        item3.details = "Oh man, this is a beautiful car.  One day I will own it!"
+        
+        ad.saveContext()
+        
+    }
     
 }
 
